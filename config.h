9feee80ae444b3fb51b7aec2cde688f5898b60ea
SamiulBasirFahim <samiulbasirfahim.rxen@gmail.com>
2024-10-14 17:16:02 +0600

    shortcut added

--- config.h
+++ config.h
@@ -200,22 +200,26 @@ static MouseShortcut mshortcuts[] = {
 
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
-#define TERMMOD (ControlMask|ShiftMask)
+#define TERMMOD (Mod4Mask|ShiftMask)
 
 static Shortcut shortcuts[] = {
-	/* mask                 keysym          function        argument */
-	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
-	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
-	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
-	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
-	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
-	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
-	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
-	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
-	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
-	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+  /* mask                 keysym          function        argument */
+  { XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
+  { ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
+  { ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
+  { XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
+  { MODKEY,               XK_equal,       zoom,           {.f = +1} },
+  { MODKEY,               XK_minus,       zoom,           {.f = -1} },
+  { MODKEY|ShiftMask,     XK_equal,       zoomreset,      {.f =  0} },
+  { ControlMask | ShiftMask,  XK_C,       clipcopy,       {.i =  0} },
+  { ControlMask | ShiftMask,  XK_V,       clippaste,      {.i =  0} },
+  { TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
+  { ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
+  { TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+  { MODKEY,               XK_k,           kscrollup,      {.i =  1} },
+  { MODKEY,               XK_j,           kscrolldown,    {.i =  1} },
+  { MODKEY,               XK_s,           changealpha,    {.f = -0.05} },
+  { MODKEY,               XK_a,           changealpha,    {.f = +0.05} },
 };
 
 /*
--- x.c
+++ x.c
@@ -55,6 +55,7 @@ typedef struct {
 static void clipcopy(const Arg *);
 static void clippaste(const Arg *);
 static void numlock(const Arg *);
+static void changealpha(const Arg *);
 static void selpaste(const Arg *);
 static void zoom(const Arg *);
 static void zoomabs(const Arg *);
@@ -294,6 +295,26 @@ numlock(const Arg *dummy)
 {
 	win.mode ^= MODE_NUMLOCK;
 }
+
+
+void
+changealpha(const Arg *arg)
+{
+     if(alpha > 1 && arg->f == 2 )
+      alpha = 1;
+    if((alpha > 0 && arg->f < 0) || (alpha < 1 && arg->f > 0))
+        alpha += arg->f;
+    if(alpha < 0)
+        alpha = 0;
+    if(alpha > 1)
+        alpha = 1;
+
+    xloadcols();
+    redraw();
+}
+
+
+
 
 void
 zoom(const Arg *arg)
